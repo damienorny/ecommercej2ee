475a3d74bd5fb27709e6362c399730efbef1d2f3
damien <damienorny@gmail.com>
2015-03-25 09:05:13 +0100

    Ajout fichier SQL pour créer la base

---
+++ divers/baseEcommerce.sql
@@ -0,0 +1,183 @@
+
+-- phpMyAdmin SQL Dump
+-- version 4.3.12
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mar 24 Mars 2015 à 16:28
+-- Version du serveur :  10.0.17-MariaDB-log
+-- Version de PHP :  5.6.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `ecommercej2ee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `article`
+--
+
+CREATE TABLE IF NOT EXISTS `article` (
+  `id_article` int(11) NOT NULL,
+  `nom_article` varchar(255) NOT NULL,
+  `desc_article` text NOT NULL,
+  `prix_article` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id_category` int(11) NOT NULL,
+  `nom_category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category_article`
+--
+
+CREATE TABLE IF NOT EXISTS `category_article` (
+  `id_category` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int(11) NOT NULL,
+  `nom_client` varchar(255) NOT NULL,
+  `prenom_client` varchar(255) NOT NULL,
+  `email_client` varchar(255) NOT NULL,
+  `adresse_client` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id_commande` int(11) NOT NULL,
+  `date_commande` datetime NOT NULL,
+  `prix_commande` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande_article`
+--
+
+CREATE TABLE IF NOT EXISTS `commande_article` (
+  `id_commande` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id_article`);
+
+--
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
+
+--
+-- Index pour la table `category_article`
+--
+ALTER TABLE `category_article`
+  ADD PRIMARY KEY (`id_category`,`id_article`), ADD KEY `id_article` (`id_article`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`), ADD UNIQUE KEY `email_client` (`email_client`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`id_commande`), ADD KEY `id_client` (`id_client`);
+
+--
+-- Index pour la table `commande_article`
+--
+ALTER TABLE `commande_article`
+  ADD PRIMARY KEY (`id_commande`,`id_article`), ADD KEY `id_article` (`id_article`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id_article` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `category_article`
+--
+ALTER TABLE `category_article`
+ADD CONSTRAINT `category_article_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`),
+ADD CONSTRAINT `category_article_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`);
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`);
+
+--
+-- Contraintes pour la table `commande_article`
+--
+ALTER TABLE `commande_article`
+ADD CONSTRAINT `commande_article_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id_commande`),
+ADD CONSTRAINT `commande_article_ibfk_2` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
