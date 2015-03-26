5e4482748d184166f89725ad3f625df07ea9e981
damien <damienorny@gmail.com>
2015-03-26 10:31:20 +0100

    Ajout gestion base de données

--- src/java/MainServlet.java
+++ src/java/MainServlet.java
@@ -6,10 +6,12 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modele.EcommerceBean;
 
 /**
  *
@@ -29,17 +31,14 @@ public class MainServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        EcommerceBean ecommerceBean = new EcommerceBean();
+        ArrayList<String> liste = ecommerceBean.getArticles();
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MainServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MainServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            for(String nom: liste)
+            {
+                out.println(nom);
+            }
         }
     }
 
--- src/java/modele/EcommerceBean.java
+++ src/java/modele/EcommerceBean.java
@@ -5,10 +5,68 @@
  */
 package modele;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author damien
  */
 public class EcommerceBean {
+    private Connection connection;
     
+    /**
+     *
+     */
+    public void connectBDD()
+    {
+        try {
+        // This loads an instance of the MySQL Driver.
+        // The driver has to be in the classpath.
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException cnfe){
+            System.out.println("" + cnfe);
+        }
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommercej2ee?zeroDateTimeBehavior=convertToNull", "root", "Paradise");
+        } catch (SQLException ex) {
+            System.out.println("" + ex);
+        }
+    }
+    
+    /**
+     *
+     */
+    public void disconnectBDD()
+    {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(EcommerceBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ArrayList<String> getArticles()
+    {
+        ArrayList<String> liste = new ArrayList();
+        connectBDD();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM article");
+            while(rs.next())
+            {
+                liste.add(rs.getString("nom_article"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EcommerceBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        disconnectBDD();
+        return liste;
+    }
 }
