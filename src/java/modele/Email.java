9981e651d6f69ae9d5b8ea016312f0853d979c2e
Mat2893 <mathieu.gerard@edu.esiee.fr>
2015-03-30 14:42:18 +0200

    Ajout Email.java installer javamail

---
+++ src/java/modele/Email.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+/**
+* Simple demonstration of using the javax.mail API.
+*
+* Run from the command line. Please edit the implementation
+* to use correct email addresses and host name.
+*/
+public final class Email 
+{
+
+    private final static String MAILER_VERSION = "Java";
+    
+    //Email email = new Email();
+    //email.envoyerMailSMTP("to", "objet", "body");
+    
+    public static boolean envoyerMailSMTP(String to, String objet, String body) 
+    {
+        final String username="mathieu.gerard@edu.esiee.fr";
+        final String password="Nico2893!!!";
+        Properties prop=new Properties();
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getDefaultInstance(prop,new javax.mail.Authenticator() 
+        {
+            protected PasswordAuthentication getPasswordAuthentication() 
+            {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+        try {
+            String htmlBody = "<strong>This is an HTML Message</strong>";
+            String textBody = body;
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("zozo@zozo.com"));
+            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(to));
+            message.setSubject(objet);
+            MailcapCommandMap mc = (MailcapCommandMap) CommandMap.getDefaultCommandMap();
+            mc.addMailcap("text/html;; x-java-content-handler=com.sun.mail.handlers.text_html");
+            mc.addMailcap("text/xml;; x-java-content-handler=com.sun.mail.handlers.text_xml");
+            mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain");
+            mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
+            mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822");
+            CommandMap.setDefaultCommandMap(mc);
+            message.setText(htmlBody);
+            message.setContent(textBody, "text/html");
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
+
