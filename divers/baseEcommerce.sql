d4b30600f3692c0a800c7614b84860e1e6b1e293
alexnadre <alexandre.lloret@edu.esiee.fr>
2015-03-30 18:45:43 +0200

    Recherche

--- divers/baseEcommerce.sql
+++ divers/baseEcommerce.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Lun 30 Mars 2015 à 18:43
+-- Généré le: Lun 30 Mars 2015 à 18:45
 -- Version du serveur: 5.5.41-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.7
 
@@ -23,6 +23,100 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `article`
+--
+
+CREATE TABLE IF NOT EXISTS `article` (
+  `id_article` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_article` varchar(255) NOT NULL,
+  `desc_article` text NOT NULL,
+  `prix_article` float NOT NULL,
+  `src_article` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_article`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `article`
+--
+
+INSERT INTO `article` (`id_article`, `nom_article`, `desc_article`, `prix_article`, `src_article`) VALUES
+(1, 'Article 1', 'Article 1', 12, ''),
+(2, 'Article 2', 'Article 2', 1212, ''),
+(3, 'Clavier', '', 9.99, 'http://images.grosbill.com/imagesproduitnew/imagesgallery/BIG/102873.jpg'),
+(4, 'Ordinateur Portable', 'Alienware modéle M17X', 1299, 'http://www.notebookcheck.biz/uploads/tx_nbc2/awareM17X.jpg'),
+(5, 'Voiture Électrique', 'Renault Zoe', 19000, 'http://www.avem.fr/img/vep/zoe_ze.jpg'),
+(6, 'Souris Informatique', 'Souris Gamer avec fil', 60, 'http://media.ldlc.com/ld/products/00/01/32/13/LD0001321389_1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id_category` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_category` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `category`
+--
+
+INSERT INTO `category` (`id_category`, `nom_category`) VALUES
+(1, 'Categorie1'),
+(3, 'Categorie2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category_article`
+--
+
+CREATE TABLE IF NOT EXISTS `category_article` (
+  `id_category` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  PRIMARY KEY (`id_category`,`id_article`),
+  KEY `id_article` (`id_article`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `category_article`
+--
+
+INSERT INTO `category_article` (`id_category`, `id_article`) VALUES
+(1, 1),
+(3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_client` varchar(255) NOT NULL,
+  `prenom_client` varchar(255) NOT NULL,
+  `email_client` varchar(255) NOT NULL,
+  `mdp_client` varchar(255) NOT NULL,
+  `adresse_client` text NOT NULL,
+  PRIMARY KEY (`id_client`),
+  UNIQUE KEY `email_client` (`email_client`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `client`
+--
+
+INSERT INTO `client` (`id_client`, `nom_client`, `prenom_client`, `email_client`, `mdp_client`, `adresse_client`) VALUES
+(1, 'damien', 'damien', 'damien@aa.com', 'damien', 'damien'),
+(2, 'damien', 'damien', 'damien2@aa.com', 'damien', 'damien'),
+(3, 'Lloret', 'Alexandre', 'alexll', 'paz', 'jj');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `commande`
 --
 
@@ -35,15 +129,43 @@ CREATE TABLE IF NOT EXISTS `commande` (
   KEY `id_client` (`id_client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande_article`
+--
+
+CREATE TABLE IF NOT EXISTS `commande_article` (
+  `id_commande` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`id_commande`,`id_article`),
+  KEY `id_article` (`id_article`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Contraintes pour les tables exportées
 --
+
+--
+-- Contraintes pour la table `category_article`
+--
+ALTER TABLE `category_article`
+  ADD CONSTRAINT `category_article_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`),
+  ADD CONSTRAINT `category_article_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`);
 
 --
 -- Contraintes pour la table `commande`
 --
 ALTER TABLE `commande`
   ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`);
+
+--
+-- Contraintes pour la table `commande_article`
+--
+ALTER TABLE `commande_article`
+  ADD CONSTRAINT `commande_article_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id_commande`),
+  ADD CONSTRAINT `commande_article_ibfk_2` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
